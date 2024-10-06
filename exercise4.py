8a9c3e52fbe448226de9e02631075ce0022cfe51
edmharx <edmharxx@gmail.com>
2024-10-06 19:47:06 +0800

    debug

--- exercise4.py
+++ exercise4.py
@@ -2,5 +2,5 @@ height = int(input("Enter the height of the triangle: "))
 
 for i in range(height):
     new_height = height-1
-    print("*"*(new_height))
+    print("*"*(new_height+1))
     height = new_height
