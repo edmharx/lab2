d44f804e217fc16808138c39eaaee23b3fa25cc5
edmharx <edmharxx@gmail.com>
2024-10-06 19:26:05 +0800

    printing of sides

--- exercise3.py
+++ exercise3.py
@@ -3,4 +3,5 @@ length = int(input("Input the length of the sides: "))
 for i in range(length):
     if i == length or i == 0:
         print("x"*length)
-    
+    else:
+        print("x"+" "*(length-2)+"x")
