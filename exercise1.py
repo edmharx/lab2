1480987bc22d097ab08f346653af2e82df110864
edmharx <edmharxx@gmail.com>
2024-10-06 17:07:58 +0800

    "call the fun then fix bug"

--- exercise1.py
+++ exercise1.py
@@ -2,9 +2,10 @@ num = int(input("Put the whole number: "))
 exponent = int(input("Put the exponent: "))
 
 def power(num, exponent):
-    if exponent == 1:
+    if exponent == 0:
         return 1
     else:
         return num * power(num, exponent-1)
 
-power(num, exponent)
+ans = power(num, exponent)
+print(ans)
