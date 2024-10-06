d436b81344b8d5142c2111a790790bb8e9da8325
edmharx <edmharxx@gmail.com>
2024-10-06 19:27:17 +0800

    Fix bug

--- exercise3.py
+++ exercise3.py
@@ -1,7 +1,7 @@
 length = int(input("Input the length of the sides: "))
 
 for i in range(length):
-    if i == length or i == 0:
+    if i == length-1 or i == 0:
         print("x"*length)
     else:
         print("x"+" "*(length-2)+"x")
